17208d9f46045516668c1d38bc13d1dec29ff18a
yeawonbong <ybong.develop@gmail.com>
2021-09-09 17:36:09 +0900

    0906

--- ms_fill_data.c
+++ ms_fill_data.c
@@ -2,11 +2,6 @@
 
 void	ft_filldata(t_data *data, char **envp) //이름수정할래
 {
-	// -3 / 3
-	// -
-	// -
-	// - NULL
-	//복사
 	int		i;
 
 	i = 0;
@@ -25,19 +20,6 @@ void	ft_filldata(t_data *data, char **envp) //이름수정할래
 	data->stdio[1] = dup(STDOUT_FILENO);
 	data->pipe_flag = 0;
 	data->redirect_flag = 0;
-	// printf("iii=%d\n", i);
-	// // ft_copy_env(envp, data->env);
 	ft_sort_env(data);
 	return ;
 }
-
-// void	ft_unset()
-// {
-// }
-
-
-// void	ft_exit()
-// {
-// }
-
-//export set과 unset, exit
